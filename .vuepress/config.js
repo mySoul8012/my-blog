47943937e6f123f5eca592942766c26c4533b9b6
mySoul <mingming@mingming.email>
2025-04-03 19:10:14 +0800

    Update config.js

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     theme: 'blog',
-    title: '我的博客',
-    description: '欢迎来到我的个人博客',
+    title: '我的课程集散地',
+    description: '欢迎来到我的课程集散地',
     themeConfig: {
         // 在此处添加主题配置，例如导航栏、侧边栏等
     }
